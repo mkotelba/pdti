b8af54bdd25d92d01b97f9bb01d3041107c96fe3
Brian Harris <sendbrianemail@gmail.com>
2015-01-27 13:49:44 -0500

    Removed federation classes as they are now generated via the FederationStructures.xsd in the pdti-ws project. Enabled WS Addressing on the endpoint. Added target namespace to endpoint so the the response is dsml:batchResponse rather hpd:batchResponse

--- pdti-server/src/main/java/gov/hhs/onc/pdti/ws/impl/ProviderInformationDirectoryImpl.java
+++ pdti-server/src/main/java/gov/hhs/onc/pdti/ws/impl/ProviderInformationDirectoryImpl.java
@@ -6,11 +6,15 @@ import gov.hhs.onc.pdti.service.DirectoryService;
 import gov.hhs.onc.pdti.ws.api.BatchRequest;
 import gov.hhs.onc.pdti.ws.api.BatchResponse;
 import gov.hhs.onc.pdti.ws.api.ProviderInformationDirectoryPortType;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
+import javax.xml.ws.soap.Addressing;
+import javax.xml.ws.soap.AddressingFeature.Responses;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
@@ -20,11 +24,12 @@ import org.springframework.stereotype.Service;
 @Service("providerInfoDir")
 @SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
 @WebService(wsdlLocation = "META-INF/wsdl/HPD_ProviderInformationDirectory.wsdl", serviceName = "ProviderInformationDirectory_Service", targetNamespace = "urn:ihe:iti:hpd:2010", portName = "ProviderInformationDirectory_Port_Soap")
+@Addressing(enabled=true, required=true, responses = Responses.ANONYMOUS)
 public class ProviderInformationDirectoryImpl extends AbstractProviderInformationDirectory<BatchRequest, BatchResponse> implements
         ProviderInformationDirectoryPortType {
     @Override
-    @WebMethod(operationName = "ProviderInformationQueryRequest")
-    @WebResult(name = "batchResponse")
+    @WebMethod(operationName = "ProviderInformationQueryRequest", action = "urn:ihe:iti:2010:ProviderInformationQuery")
+    @WebResult(name = "batchResponse", targetNamespace = "urn:oasis:names:tc:DSML:2:0:core")
     public BatchResponse providerInformationQueryRequest(
             @WebParam(name = "batchRequest") BatchRequest queryRequest) {
         return this.dirService.processRequest(queryRequest);
